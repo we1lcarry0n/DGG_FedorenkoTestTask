044d681c98ff4468b567ebfa46b7b3ab3271a0ca
Andrii <47238307+we1lcarry0n@users.noreply.github.com>
2025-10-07 20:39:44 +0300

    task 1 implemented

---
+++ Assets/Scripts/Helpers/TargetRotation.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class TargetRotation : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private bool _rotateCurved;
+
+    private float _currentYOffset;
+
+    private void Start()
+    {
+        _currentYOffset = transform.position.y;
+    }
+
+    private void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, _rotationSpeed * Time.deltaTime);
+        if (_rotateCurved)
+        {
+            ApplySinFluctuation();
+        }
+    }
+
+    private void ApplySinFluctuation()
+    {
+        float sin = Mathf.Sin((transform.rotation.eulerAngles.y * Mathf.PI)/180*2);
+        transform.position = new Vector3(0, sin + _currentYOffset, 0);
+    }
+}
---
+++ Assets/Scripts/Task_01/AutoLookAt.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+
+public class AutoLookAt : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private bool _lockYAxis;
+
+    private Transform _target;
+    private Vector3 _targetPosition;
+
+    private void Start()
+    {
+        _target = GameObject.FindGameObjectWithTag("Target").transform;
+    }
+
+    private void Update()
+    {
+        _targetPosition = _lockYAxis ? new Vector3(_target.position.x, 0f, _target.position.z) : _target.position - transform.position;
+        Quaternion rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(_targetPosition), _rotationSpeed * Time.deltaTime);
+        transform.rotation = rotation;
+    }
+}
